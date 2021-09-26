b8fccd0951606a66795afe544afc697e8f4a36f1
Eric Quere <13007637+ericqu@users.noreply.github.com>
2021-09-26 23:34:14 +0200

    docu 16

--- docs/make.jl
+++ docs/make.jl
@@ -3,13 +3,12 @@ using Documenter, LinearRegression, StatsModels, DataFrames
 
 makedocs(sitename="LinearRegression.jl")
 
-# deploydocs(
-#     repo = "github.com/ericqu/LinearRegression.jl.git",
-#     push_preview = true,
-#     devbranch = "main",
-#     devurl = "dev",
-#     versions = nothing,
-# )
 deploydocs(
-    repo = "github.com/ericqu/LinearRegression.jl.git"
-    )
+    repo = "github.com/ericqu/LinearRegression.jl.git",
+    push_preview = false,
+    devbranch = "main",
+    devurl = "dev",
+)
+# deploydocs(
+#     repo = "github.com/ericqu/LinearRegression.jl.git"
+#     )
